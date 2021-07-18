1ebfd80d1a144029e8a414ca7e02c791de20a0d2
HARIPRASAD S A <myharicode@gmail.com>
2021-07-18 11:35:29 +0530

    Histogram all concepts

--- plotlyBasics/box_plot_exercise.py
+++ plotlyBasics/box_plot_exercise.py
@@ -5,9 +5,9 @@ import numpy as np
 
 df = pd.read_csv("../Data/abalone.csv")
 
-d = np.random.choice(df['rings'],10,replace=False)
-d2 = np.random.choice(df['rings'],10,replace=False)
+d = np.random.choice(df['rings'], 10, replace=False)
+d2 = np.random.choice(df['rings'], 10, replace=False)
 
-data = [go.Box(y=d,name='Data'),
-        go.Box(y=d2,name="data2")]
+data = [go.Box(y=d, name='Data'),
+        go.Box(y=d2, name="data2")]
 pyo.plot(data)
--- plotlyBasics/histogram_exercise.py
+++ plotlyBasics/histogram_exercise.py
@@ -2,9 +2,9 @@ import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
-df = pd.read_csv('../Data/mpg.csv')
+df = pd.read_csv('../Data/abalone.csv')
 
-data = [go.Histogram(x=df['mpg'], xbins=dict(start=0, end=50,size=10))]
+data = [go.Histogram(x=df['length'], xbins=dict(start=0, end=1, size=0.02))]
 layout = go.Layout(title='Histogram')
 fig = go.Figure(data=data, layout=layout)
 pyo.plot(fig)
